13d982c6ccf1ec4f8b126f9b35f62c2750923a39
Patrick Piemonte <piemonte@alumni.cmu.edu>
2014-06-24 09:44:19 -0700

    LoginViewController, add basic version of amir's controller

---
+++ Source/NLSLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  NLSLoginViewController.h
+//  App
+//
+//  Created by 4m1r on 6/18/14.
+//  Copyright (c) 2014 Slyce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NLSLoginViewController : UIViewController
+
+@end
---
+++ Source/NLSLoginViewController.m
@@ -0,0 +1,35 @@
+//
+//  NLSLoginViewController.m
+//  App
+//
+//  Created by 4m1r on 6/18/14.
+//  Copyright (c) 2014 Slyce. All rights reserved.
+//
+
+#import "NLSLoginViewController.h"
+#import "FacebookSDK.h"
+
+@interface NLSLoginViewController ()
+{
+    UIButton *_loginButton;
+}
+
+@end
+
+@implementation NLSLoginViewController
+
+#pragma mark - view lifecycle
+
+- (void)viewDidLoad
+{
+
+}
+
+#pragma mark - gesture recognizers
+
+- (void)_handleButtonPress:(UIButton *)button
+{
+
+}
+
+@end
