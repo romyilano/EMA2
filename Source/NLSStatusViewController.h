52663c0384a8846ce93ff0fad71f7d9e9a25ca66
Patrick Piemonte <piemonte@alumni.cmu.edu>
2014-06-25 10:58:18 -0700

    skeleton controllers for bill creation and main status screen

---
+++ Source/NLSStatusViewController.h
@@ -0,0 +1,13 @@
+//
+//  NLSStatusViewController.h
+//  App
+//
+//  Created by Patrick Piemonte on 6/25/14.
+//  Copyright (c) 2014 Slyce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NLSStatusViewController : UIViewController
+
+@end
