aa929ed90e6427d40496ae6d2a6220cc596bb43d
Amir Djavaherian <a_mir@icloud.com>
2014-08-22 15:33:42 -0700

    stable. first rev to testers.

--- App/TODO.h
+++ App/TODO.h
@@ -15,6 +15,8 @@
  test on multi-device
  add mesh terms at bottom of abstract
  MR and CT and ST caps in Titles
+ Link to this literature on EBM http://www.ncbi.nlm.nih.gov/pmc/articles/PMC4076127/
+ Add links from all articles to PMC http://www.ncbi.nlm.nih.gov/pmc/articles/pmid/901087/citedby/?tool=pubmed
  
  
  */
