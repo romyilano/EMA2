9da123aafc404874d8d3e44a2e982218daa07fdd
Amir Djavaherian <a_mir@icloud.com>
2014-10-08 10:01:48 -0700

    Installed on two testers.

--- App/TODO.h
+++ App/TODO.h
@@ -1,24 +1,17 @@
 /*
  
- 
- TODOs: 
- 
- 
- About tab
+ TODOs:
+
  Advanced Search
- Fonts in search, nav
  Colors
  Abstract Header
  Header Art
  Art for ICONS / Splash
  test on multi-device
- add mesh terms at bottom of abstract
  MR MRI and CT and ST, WHO, UK, ED 3D CPR caps in Titles
  JAMA, QJM in Journals
- Link to this literature on EBM http://www.ncbi.nlm.nih.gov/pmc/articles/PMC4076127/
- Add links from all articles to PMC http://www.ncbi.nlm.nih.gov/pmc/articles/pmid/901087/citedby/?tool=pubmed
  WebView open in Safari option
  springer links http://link.springer.com/journal/10654/12/6/page/1
  Add scholar links http://scholar.google.com  Sci Justice. 2001 Jul-Sep;41(3):197-9
- 
+
  */
